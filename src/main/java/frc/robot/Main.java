5a59afb7ec677cf33b6aaebac53d39242eb7ee45
Quake - FRC Team 9203 <120520192+Quake9203@users.noreply.github.com>
2023-11-24 13:34:15 -0500

    Update Main.java
    fix comment

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-## Primary Script that runs all others
+// Primary Script that runs all others
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
